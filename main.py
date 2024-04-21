8258eb31a911394e09b297616bb387ab77f77061
baydinvladislav <baydinvladislav@yandex.ru>
2024-04-21 13:51:59 +0300

    /privacy-policy endpoint

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ application = Flask(__name__)
 
 @application.route("/")
 def get_root_page():
-    return render_template('index.html')
+    return render_template('main.html')
+
+
+@application.route("/privacy-policy")
+def get_privacy_policy():
+    return render_template('privacy_policy.html')
 
 
 if __name__ == "__main__":
