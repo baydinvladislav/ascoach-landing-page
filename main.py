7fea79e14dc31120dcc951d4d4b7849f99566580
baydinvladislav <baydinvladislav@yandex.ru>
2024-04-21 13:32:25 +0300

    reformat file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 
-
 application = Flask(__name__)
 
 
 @application.route("/")
 def get_root_page():
-   return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
